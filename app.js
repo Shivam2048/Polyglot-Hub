4e646d97ffe064c09595b9dccaaecc30cbf9d614
Shivam <shivamkr2048@gmail.com>
2025-01-12 12:26:49 +0530

    Build header with logo and navigation

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-import express from express;
+import express from 'express';
 import axios from 'axios';
 import bodyParser from 'body-parser';
 
 const app= express();
 const port= 2000;
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.use('/', (req,res) => {
+    res.render('home.ejs');
+});
+
+app.use('/home', (req,res) => {
+    res.render('home.ejs');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
